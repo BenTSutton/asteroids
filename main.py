f690278ab35893d5710e45b88fd4fc9faf92f446
BenTSutton <BenThomasSuttonCode@gmail.com>
2025-01-06 15:49:28 +0000

    rendered a triangle on the screen and created a player class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import * 
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -8,7 +9,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -16,8 +17,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)
